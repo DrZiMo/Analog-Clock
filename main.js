bcbcc7cd780d74b0e8d08e7c6b49fb6fa6f14aea
drzimo <zuhaybhamar@gmail.com>
2024-09-01 14:37:13 +0300

    The Primary structure is done !

---
+++ main.js
@@ -0,0 +1,25 @@
+const second = document.querySelector('.second');
+const minute = document.querySelector('.minutes');
+const hour = document.querySelector('.hours');
+
+function updateTheClock() {
+    const date = new Date();
+
+    const currentHour = date.getHours();
+    const currentMin = date.getMinutes();
+    const currentSec = date.getSeconds();
+
+    rotateTheArrows(currentHour, currentMin, currentSec);
+
+    setTimeout(updateTheClock, 1000);
+}
+
+function rotateTheArrows(h, m, s) {
+    let secondDeg = s * 6;
+    let minDeg = m * 6;
+    let hourDeg = h * 30;
+
+    second.style.transform = `rotate(${secondDeg}deg)`;
+    minute.style.transform = `rotate(${minDeg}deg)`;
+    hour.style.transform = `rotate(${hourDeg}deg)`;
+}
